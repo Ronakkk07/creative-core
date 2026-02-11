cc100a65afe2319a0d99fd2ac2ead76795aa7761
Ronakkk07 <ronakrajput1106@gmail.com>
2026-02-11 17:51:36 +0000

    updated frontend api urls to render backend

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000/api";
+export const API_BASE_URL = "https://portfolio-backend-9r7n.onrender.com";
 
 export async function fetchHeroData() {
   const res = await fetch(`${API_BASE_URL}/hero/`);
@@ -7,25 +7,25 @@ export async function fetchHeroData() {
 }
 
 export async function fetchProjects() {
-  const res = await fetch("http://localhost:8000/api/projects/");
+  const res = await fetch(`${API_BASE_URL}/hero/`);
   if (!res.ok) throw new Error("Failed to fetch projects");
   return res.json();
 }
 
 export async function fetchExperience() {
-  const res = await fetch("http://localhost:8000/api/experience/");
+  const res = await fetch(`${API_BASE_URL}/experience/`);
   if (!res.ok) throw new Error("Failed to fetch experience");
   return res.json();
 }
 
 export async function fetchContactInfo() {
-  const res = await fetch("http://localhost:8000/api/contact/info/");
+  const res = await fetch(`${API_BASE_URL}/contact/info/`);
   if (!res.ok) throw new Error("Failed");
   return res.json();
 }
 
 export async function sendContactMessage(data: any) {
-  const res = await fetch("http://localhost:8000/api/contact/message/", {
+  const res = await fetch(`${API_BASE_URL}/contact/message/`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
@@ -36,13 +36,13 @@ export async function sendContactMessage(data: any) {
 }
 
 export async function fetchSkills() {
-  const res = await fetch("http://localhost:8000/api/skills/");
+  const res = await fetch(`${API_BASE_URL}/skills/`);
   if (!res.ok) throw new Error("Failed to fetch skills");
   return res.json();
 }
 
 export async function fetchAbout() {
-  const res = await fetch("http://localhost:8000/api/about/");
+  const res = await fetch(`${API_BASE_URL}/about/`);
   if (!res.ok) throw new Error("Failed to fetch about");
   return res.json();
 }
